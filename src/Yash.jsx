8f85c2e9d0ba5d7dfa752fed6d94dc4bd4f00187
Yash-Nathile <yashnathile1@gmail.com>
2025-03-13 02:30:01 +0530

    Add files via upload

---
+++ src/Yash.jsx
@@ -0,0 +1,100 @@
+// import React from "react";
+// import Component1 from "./component1";
+// import Component2 from "./component2";
+// import Component3 from "./component3";
+// import Component4 from "./component4";
+// import Component5 from "./component5";
+
+// const Yash = () => {
+//     return (
+//       <div>
+//         <Component1/><hr/>
+//         <Component2/><hr/>
+//         <Component3/><hr/>
+//         <Component4/><hr/>
+//         <Component5/><hr/>
+//       </div>
+//     );
+//   };
+
+//   export default Yash;
+
+
+
+
+
+
+// import React from "react";
+// import Component1 from "./component1";
+// import Component2 from "./component2";
+// import {players} from "./component3";
+// import {ProductCard} from "./component4";
+// import Component5 from "./component5";
+
+// const Yash = () => {
+//     return (
+//       <div>
+//         <Component1/><hr/>
+//         <Component2/><hr/>
+//         <players/><hr/>
+//         <ProductCard/><hr/>
+//         <Component5/><hr/>
+//       </div>
+//     );
+//   };
+
+//   export default Yash;
+
+
+
+// import React from "react";
+// import UseState from "./UsesState";
+
+// const Yash = () => {
+//       return (
+//         <div>
+//           <UseState/><hr/>
+//         </div>
+//       );
+//     };
+  
+//     export default Yash;
+  
+
+
+
+
+// import React from "react";
+// import Book from "./Book";
+
+// const Yash = () => {
+//       return (
+//         <div>
+//           <Book/><hr/>
+//         </div>
+//       );
+//     };
+  
+//     export default Yash;
+  
+
+
+import React from "react";
+import Courses from "./component/Courses";
+import Students from "./component/Students";
+import Smartphnes from "./component/Smartphones";
+import Anime from "./component/Anime";
+import Laptop from "./component/Laptop";
+const Yash = () => {
+      return (
+        <div>
+          <Courses/><hr/>
+          <Students/><hr/>
+          <Smartphnes/><hr/>
+          <Anime/><hr/>
+          <Laptop/><hr/>
+        </div>
+      );
+    };
+export default Yash;
+
